c33a9138f0c08f26f3d1d2c5e779938a10c8aa5a
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-08-31 14:08:55 +0300

    Взаимодействие объектов [#166087]

---
+++ src/main/java/ru/job4j/tracker/oop/BallStory.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        Ball ball = new Ball();
+        System.out.println("Встретил зайца: ");
+        hare.tryEat(ball);
+        System.out.println(", встретил волка: ");
+        wolf.tryEat(ball);
+        System.out.println(", а при встрече с лисой: ");
+        fox.tryEat(ball);
+    }
+}
