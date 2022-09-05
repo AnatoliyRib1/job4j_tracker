02560c25713b4596a0b709b5b9d7f6a7a6c8b00c
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-09-05 15:12:11 +0300

    Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book cleanCode = new Book("Clean Code", 450);
+        Book tomSoyer = new Book("Tom Soyer", 500);
+        Book totalWar = new Book("Total War", 150);
+        Book knife = new Book("Knife", 333);
+        Book[] books = new Book[4];
+        books[0] = cleanCode;
+        books[1] = tomSoyer;
+        books[2] = totalWar;
+        books[3] = knife;
+        for (int i = 0; i < books.length; i++) {
+            Book bookName = books[i];
+            System.out.println(bookName.getName() + " имеет всего " + bookName.getPages() + " страниц.");
+
+
+        }
+        System.out.println("\nReplace Clean Code to Knife.\n");
+        Book[] change = new Book[1];
+        change[0] = books[0];
+        books[0] = books[3];
+        books[3] = change[0];
+        for (int i = 0; i < books.length; i++) {
+            Book bookName = books[i];
+            System.out.println(bookName.getName() + " имеет всего " + bookName.getPages() + " страниц.");
+        }
+        for (int i = 0; i < books.length; i++) {
+            if (books[i].getName().equals("Clean Code")) {
+                System.out.println("\n" + books[i].getName() + " имеет всего " + books[i].getPages() + " страниц.");
+            }
+        }
+    }
+}
