cc16530285ccabf5288ba07aae2a0a1ba6304fc8
AnatoliyRib1 <anatoliy.rib@yandex.ru>
2022-09-28 11:26:23 +0300

    1. Singleton [#94606 #345515]

--- src/main/java/ru/job4j/SingleTracker.java
+++ src/main/java/ru/job4j/SingleTracker.java
@@ -3,10 +3,20 @@ package ru.job4j;
 import ru.job4j.tracker.Item;
 import ru.job4j.tracker.Tracker;
 
-public class SingleTracker {
+public final class SingleTracker {
     private Tracker tracker = new Tracker();
 
+    private static SingleTracker instance = null;
+
     private SingleTracker() {
+
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
     }
 
     public Item add(Item item) {
